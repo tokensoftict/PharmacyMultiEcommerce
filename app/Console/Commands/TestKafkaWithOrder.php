f0125a76268f8dec3e5e512dc1fc3a103372f01a
Yusuf Olatunji <yoyforever@gmail.com>
2025-05-01 00:08:41 +0100

    sample order

---
+++ app/Console/Commands/TestKafkaWithOrder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\KafkaAction;
+use App\Enums\KafkaEvent;
+use App\Enums\KafkaTopics;
+use App\Models\Order;
+use Illuminate\Console\Command;
+use Junges\Kafka\Facades\Kafka;
+use Junges\Kafka\Message\Message;
+use function Laravel\Prompts\text;
+class TestKafkaWithOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-kafka-with-order';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $orderId = text(
+            label: 'Enter the Order Id, you want to Push?',
+            placeholder: 'E.g. 1,2,3',
+            hint: 'The Order Id now..........'
+        );
+
+        $order = Order::find($orderId);
+
+        $message = new Message(
+            headers: ['event' => KafkaEvent::ONLINE_PUSH],
+            body: ['order' => $order->toArray(), "action" => KAFKAAction::PROCESS_ORDER],
+            key: config('app.KAFKA_HEADER_KEY')
+        );
+
+        Kafka::publish()->onTopic(KafkaTopics::ORDERS)->withMessage($message)->send();
+
+        $this->info('Order Id: ' . $orderId);
+    }
+}
