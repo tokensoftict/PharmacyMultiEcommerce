4da5c5b100613ea902c990c00b7679f9d3fd947e
Yusuf Olatunji <yoyforever@gmail.com>
2025-05-22 18:55:39 +0100

    adding sendChamp SMS notification]

--- app/Classes/Notification/SmsNotification.php
+++ app/Classes/Notification/SmsNotification.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Classes\Notification;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
@@ -8,17 +9,51 @@ use Illuminate\Support\Facades\Storage;
 class SmsNotification
 {
 
+    /**
+     * @throws ConnectionException
+     */
     public function send($notifiable, Notification $notification)
     {
         $message = $notification->toSms($notifiable);
         Storage::append("sms.txt", $message);
-        Http::post(config("app.BULKSMS_URL"), [
-            "email" => config("app.BULKSMS_EMAIL"),
-            "password" => config("app.BULKSMS_PASSWORD"),
-            "recipient" => $notifiable->phone,
-            "message" => $message,
-            "senderid" => config("app.BULKSMS_SENDER"),
-        ])->getBody()->getContents();
+        if(config('app.BULKSMS_ENGINE') === "SENDCHAMP") {
+
+            $response = Http::withHeaders([
+                'Accept' => 'application/json,text/plain,*/*',
+                'Authorization' => config('app.SEND_CHAMP_AUTHORIZATION'),
+                'Content-Type' => 'application/json',
+            ])
+                ->timeout(30)
+                ->post(config("app.BULKSMS_URL"), [
+                'channel' => 'sms',
+                'sender' => config("app.BULKSMS_SENDER"),
+                'token_type' => 'numeric',
+                'token_length' => config("app.SEND_CHAMP_TOKEN_LENGTH"),
+                'expiration_time' => config("app.SEND_CHAMP_EXPIRATION_TIME"),
+                'customer_mobile_number' => $notifiable->phone,
+                'meta_data' => null,
+                'in_app_token' => false,
+            ]);
+
+            if ($response->successful()) {
+                $body = json_decode($response->body(), true);
+                $token = $body['data']['token'];
+
+                $notifiable->verification_pin = $token;
+                $notifiable->update();
+            } else {
+                throw new \Exception("There was an error while sending SMS, please try again later");
+            }
+
+        } else {
+            Http::post(config("app.BULKSMS_URL"), [
+                "email" => config("app.BULKSMS_EMAIL"),
+                "password" => config("app.BULKSMS_PASSWORD"),
+                "recipient" => $notifiable->phone,
+                "message" => $message,
+                "senderid" => config("app.BULKSMS_SENDER"),
+            ])->getBody()->getContents();
+        }
         return  true;
     }
 
