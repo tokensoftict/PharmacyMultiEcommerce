c1042807f971f3507ec9b7866238f3bbc92beed2
Yusuf Olatunji <yoyforever@gmail.com>
2025-05-24 16:46:07 +0100

    updating composer and console

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command("queue:work --stop-when-empty")->everyFiveSeconds()->withoutOverlapping();
-//Schedule::command("app:send-push-notification-to-clients-phone")->everyFiveSeconds()->withoutOverlapping();
+Schedule::command("queue:work --stop-when-empty")->everyMinute()->withoutOverlapping();
+Schedule::command("app:send-push-notification-to-clients-phone")->everyMinute()->withoutOverlapping();
