a76422c1ecd924a105ba65fa448ad9d80620cd67
Yusuf Olatunji <yoyforever@gmail.com>
2025-05-24 16:12:10 +0100

    removing notifcation to phone

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command("queue:work --stop-when-empty")->everyFiveSeconds()->withoutOverlapping();
-Schedule::command("app:send-push-notification-to-clients-phone")->everyFiveSeconds()->withoutOverlapping();
+//Schedule::command("app:send-push-notification-to-clients-phone")->everyFiveSeconds()->withoutOverlapping();
