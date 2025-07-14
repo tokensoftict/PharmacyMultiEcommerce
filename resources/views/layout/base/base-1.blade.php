1d3eef7da5b166d217e09999f07e1ea56bb33c66
Yusuf Olatunji <yoyforever@gmail.com>
2025-07-14 21:45:15 +0100

    fixing rappasoft

--- resources/views/layout/base/base-1.blade.php
+++ resources/views/layout/base/base-1.blade.php
@@ -25,7 +25,7 @@
 @stack('css')
 
 @livewireStyles
-    <script src="/rappasoft/laravel-livewire-tables/core.min.js"  ></script>
-    <script src="/rappasoft/laravel-livewire-tables/thirdparty.min.js"  ></script>
+    <script src="{{ asset('rappasoft/laravel-livewire-tables/core.min.js') }}"  ></script>
+    <script src="{{ asset('rappasoft/laravel-livewire-tables/thirdparty.min.js') }}"  ></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <body>
