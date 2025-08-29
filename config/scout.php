59bb0f60426dfc5de179d2f2a846a4861a5de91b
Yusuf Olatunji <yoyforever@gmail.com>
2025-08-30 00:34:48 +0100

    re-index miliseach for stock

--- config/scout.php
+++ config/scout.php
@@ -140,9 +140,9 @@ return [
         'host' => env('MEILISEARCH_HOST', 'http://localhost:7700'),
         'key' => env('MEILISEARCH_KEY'),
         'index-settings' => [
-            // 'users' => [
-            //     'filterableAttributes'=> ['id', 'name', 'email'],
-            // ],
+             \App\Models\Stock::class => [
+                 'filterableAttributes'=> ['wholesales', 'wholesales_status', 'retail_status', 'is_wholesales', 'retail', 'admin_status', 'description', 'name', 'wholesales_status', 'retail_status'],
+             ],
         ],
     ],
 
