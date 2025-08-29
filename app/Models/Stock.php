194af1d531e618d4b51f60769109bc182d765a3b
Yusuf Olatunji <yoyforever@gmail.com>
2025-08-30 00:38:38 +0100

    re-index miliseach for stock

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -108,6 +108,8 @@ class Stock extends Model
 		'user_id'
 	];
 
+    protected $appends = ['wholesales_status', 'retail_status'];
+
     protected $with = ['stockquantityprices','stock_sizes', 'classification', 'productgroup', 'manufacturer', 'promotion_items', 'stock_media.media'];
 
 
@@ -120,7 +122,6 @@ class Stock extends Model
         if($this->wholessales_stock_prices) {
             $array['wholesales'] = $this->wholessales_stock_prices->toArray();
             $array['wholesales']['price'] = money($array['wholesales']['price']);
-            $array['wholesales_status'] = (bool)$this->wholessales_stock_prices->status;
         } else {
             $array['wholesales'] = false;
         }
@@ -128,7 +129,6 @@ class Stock extends Model
         if($this->supermarkets_stock_prices) {
             $array['retail'] = $this->supermarkets_stock_prices->toArray();
             $array['retail']['price'] = money( $array['retail']['price']);
-            $array['retail_status'] = (bool)$this->supermarkets_stock_prices->status;
         } else {
             $array['retail'] = false;
         }
