ff8816734e1f9c193f7a66ad3580aa970168b0bf
Yusuf Olatunji <yoyforever@gmail.com>
2025-09-02 18:23:38 +0100

    re-arranging stock by price lowest to highest

--- app/Http/Resources/Api/Stock/StockListJoinResource.php
+++ app/Http/Resources/Api/Stock/StockListJoinResource.php
@@ -16,7 +16,7 @@ class StockListJoinResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this->id,
+            "id" => $this->stock_id,
             "name" => $this->name,
             "price" => money($this->price),
             "price_not_formatted" => $this->price,
