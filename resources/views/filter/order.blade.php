5e0dc1c7b245eb9bd2f3dae86cb9ccc88fda5dfa
Yusuf Olatunji <yoyforever@gmail.com>
2025-09-29 11:21:03 +0100

    reseting report to today

--- resources/views/filter/order.blade.php
+++ resources/views/filter/order.blade.php
@@ -2,11 +2,11 @@
 <form  method="get" class="row">
     <div class="col-5">
         <label class="form-label" for="inputName2">From</label>
-        <input class="form-control" name="startDate" value="{{ request()->get('startDate', date('Y-m-01'))  }}" type="date" placeholder="From">
+        <input class="form-control" name="startDate" value="{{ request()->get('startDate', date('Y-m-d'))  }}" type="date" placeholder="From">
     </div>
     <div class="col-5">
         <label class="form-label" for="inputName2">To</label>
-        <input class="form-control" name="stopDate"  value="{{  request()->get('stopDate', date('Y-m-t')) }}" type="date" placeholder="To">
+        <input class="form-control" name="stopDate"  value="{{  request()->get('stopDate', date('Y-m-d')) }}" type="date" placeholder="To">
     </div>
     <div class="col-2 mt-1 d-flex">
         <button type="submit" class="btn btn-sm btn-outline-dark mt-4">Filter</button>
