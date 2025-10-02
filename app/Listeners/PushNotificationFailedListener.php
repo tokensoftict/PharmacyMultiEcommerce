d108b7fa828938e16793f2879703d79a19d52bd1
Yusuf Olatunji <yoyforever@gmail.com>
2025-10-02 13:57:40 +0100

    dumping device error

--- app/Listeners/PushNotificationFailedListener.php
+++ app/Listeners/PushNotificationFailedListener.php
@@ -21,6 +21,6 @@ class PushNotificationFailedListener
     public function handle(NotificationFailed $event): void
     {
         $report = Arr::get($event->data, 'report');
-        dd($report);
+        dump($report);
     }
 }
