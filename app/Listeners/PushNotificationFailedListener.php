388f4837d73ad12dc8a237fa850b08a6823739e7
Yusuf Olatunji <yoyforever@gmail.com>
2025-10-02 14:06:50 +0100

    solving notification issue

--- app/Listeners/PushNotificationFailedListener.php
+++ app/Listeners/PushNotificationFailedListener.php
@@ -2,8 +2,11 @@
 
 namespace App\Listeners;
 
+use App\Models\SupermarketUser;
+use App\Models\WholesalesUser;
 use Illuminate\Notifications\Events\NotificationFailed;
 use Illuminate\Support\Arr;
+use NotificationChannels\Fcm\FcmChannel;
 
 class PushNotificationFailedListener
 {
@@ -20,7 +23,31 @@ class PushNotificationFailedListener
      */
     public function handle(NotificationFailed $event): void
     {
-        $report = Arr::get($event->data, 'report');
-        dd($report->message);
+        if ($event->channel == FcmChannel::class) {
+
+            $report = Arr::get($event->data, 'report');
+
+            $target = $report->target();
+
+            $wholesale = WholesalesUser::query()->where('device_key', $target->value())
+                ->first();
+
+            $supermarket = SupermarketUser::query()->where('device_key', $target->value())
+                ->first();
+
+            if($supermarket) {
+                $supermarket->device_key = NULL;
+                $supermarket->save();
+            }
+
+            if($wholesale) {
+                $wholesale->device_key = NULL;
+                $wholesale->save();
+            }
+
+            $event->notifiable->notificationTokens()
+                ->where('push_token', $target->value())
+                ->delete();
+        }
     }
 }
