a35c80d6a7d06f1d4e668a260e4d02bf0e635c2e
Yusuf Olatunji <yoyforever@gmail.com>
2025-10-31 14:31:15 +0100

    fix

--- app/Http/Helpers/Helper.php
+++ app/Http/Helpers/Helper.php
@@ -337,7 +337,7 @@ function monthlyDateRange(){
 function getUserMenu2($app_id)
 {
     if(!in_array($app_id, [2,3]))  return "";
-    $menus = loadMenu($app_id);
+    $menus = loadMenu($app_id, true);
     $userMenus = '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle lh-1" href="#!" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-haspopup="true" aria-expanded="false"><span class="uil fs-8 me-2 uil-chart-pie"></span>Home</a>
                     <ul class="dropdown-menu navbar-dropdown-caret">
                         <li>
