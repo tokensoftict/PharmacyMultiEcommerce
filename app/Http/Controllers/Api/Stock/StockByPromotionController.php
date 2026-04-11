60e74cf572f47ac6d1fb84f59f95fcc813a1902b
Yusuf Olatunji <yoyforever@gmail.com>
2026-04-11 13:33:30 +0100

    loading promotional stocks

--- app/Http/Controllers/Api/Stock/StockByPromotionController.php
+++ app/Http/Controllers/Api/Stock/StockByPromotionController.php
@@ -9,6 +9,7 @@ use App\Models\Promotion;
 use App\Models\Stock;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Classes\ApplicationEnvironment;
 
 
 class StockByPromotionController extends ApiController
@@ -18,13 +19,14 @@ class StockByPromotionController extends ApiController
      * @param Promotion $promotion
      * @return JsonResponse
      */
-    public function __invoke(Request $request, Promotion $promotion) : JsonResponse
+    public function __invoke(Request $request, Promotion $promotion): JsonResponse
     {
         $stockIds = $promotion->promotion_items()->pluck('stock_id');
-        
-        $stocks = Stock::whereIn('id', $stockIds)
-            ->where('status_id', 1)
-            ->paginate();
+
+        $stocks = Stock::query()->select("stocks.*", ApplicationEnvironment::$stock_model_string . ".price", ApplicationEnvironment::$stock_model_string . ".quantity as quantity", ApplicationEnvironment::$stock_model_string . ".expiry_date as expiry_date")->withoutGlobalScope('filter_stocks')
+            ->join(ApplicationEnvironment::$stock_model_string, ApplicationEnvironment::$stock_model_string . ".stock_id", "=", "stocks.id")
+            ->whereIn("stocks.id", $stockIds)
+            ->orderBy(ApplicationEnvironment::$stock_model_string . ".price", "asc")->paginate(config("app.PAGINATE_NUMBER"));
 
         return $this->sendPaginatedSuccessResponse(
             StockListResource::collection($stocks)->response()->getData(true)
