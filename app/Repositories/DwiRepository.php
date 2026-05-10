b42edbd7fe92df85e46847385e2ea7e68bda6e8a
Yusuf Olatunji <yoyforever@gmail.com>
2026-05-10 21:20:41 +0100

    debug dsd

--- app/Repositories/DwiRepository.php
+++ app/Repositories/DwiRepository.php
@@ -14,34 +14,34 @@ class DwiRepository
      * @param array|null $extraData
      * @return array
      */
-    public function calculateDeliveryTotal(?array $shoppingCart, DeliveryMethod $methodOfDelivery , ?array $extraData) : array
+    public function calculateDeliveryTotal(?array $shoppingCart, DeliveryMethod $methodOfDelivery, ?array $extraData): array
     {
 
         $name = $extraData['template_settings']['name'] ?? ($extraData['template_settings']['title'] ?? null);
-
+dd($name);
         $deliverySelected = \App\Models\DeliveryWithinIlorin::where('app_id', \App\Classes\ApplicationEnvironment::$id)
             ->where('name', $name)
             ->where('status', true)
             ->first();
 
-        if(!$deliverySelected){
+        if (!$deliverySelected) {
             return [
                 'status' => false,
-                'name'=>$methodOfDelivery->name,
-                'amount'=>0,
+                'name' => $methodOfDelivery->name,
+                'amount' => 0,
                 'error' => ['Unable able to determine your delivery price, please try again!']
             ];
         }
 
         $amount = $deliverySelected->amount;
-        if($methodOfDelivery->isFreeDeliveryActive()){
+        if ($methodOfDelivery->isFreeDeliveryActive()) {
             $amount = 0;
         }
 
         return [
             'status' => true,
-            'name'=>$methodOfDelivery->name.'[ '.$deliverySelected->name.' ]',
-            'amount'=>$amount,
+            'name' => $methodOfDelivery->name . '[ ' . $deliverySelected->name . ' ]',
+            'amount' => $amount,
             'original_amount_formatted' => money($deliverySelected->amount),
             'is_free' => $methodOfDelivery->isFreeDeliveryActive(),
         ];
