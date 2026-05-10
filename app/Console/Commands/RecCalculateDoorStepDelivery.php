4ad3e1d0020a929e77a3939b4d8a8fdcd9f8b744
Yusuf Olatunji <yoyforever@gmail.com>
2026-05-10 16:46:59 +0100

    adding free delivery

--- app/Console/Commands/RecCalculateDoorStepDelivery.php
+++ app/Console/Commands/RecCalculateDoorStepDelivery.php
@@ -36,7 +36,7 @@ class RecCalculateDoorStepDelivery extends Command
                     $to = now();
 
                     $days = $to->diffInDays($from);
-                    dump($days);
+
                     if ($days <= $deliveryTownDistance->reset_time_days) {
 
                         $frequency = Str::plural($deliveryTownDistance->interval_frequency);
