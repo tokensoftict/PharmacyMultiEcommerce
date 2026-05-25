5c302434abb736a309af0866074bc127c32c1d94
Yusuf Olatunji <yoyforever@gmail.com>
2026-05-25 13:39:09 +0100

    recalculating door step delievery

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command("app:send-push-notification-to-clients-phone")->everyMinute()->withoutOverlapping();
-Schedule::command("app:import-order-from-old-server")->everyTwoMinutes()->withoutOverlapping();
+//Schedule::command("app:send-push-notification-to-clients-phone")->everyMinute()->withoutOverlapping();
+//Schedule::command("app:import-order-from-old-server")->everyTwoMinutes()->withoutOverlapping();
 Schedule::command("app:rec-calculate-door-step-delivery")->everyMinute()->withoutOverlapping();
