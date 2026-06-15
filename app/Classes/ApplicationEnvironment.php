5a1bf474501fe03204fd2b074dbc2f132c47a26d
Yusuf Olatunji <yoyforever@gmail.com>
2026-06-15 13:12:53 +0100

    alowing general drugs domain

--- app/Classes/ApplicationEnvironment.php
+++ app/Classes/ApplicationEnvironment.php
@@ -37,13 +37,15 @@ class ApplicationEnvironment
         self::$type = $application->type;
         self::$id = $application->id;
         self::$model_id = $application->model_id;
-        $subdomain = Str::before(request()->getHost(), '.'); //wholesales-admin-staging.admin.dashboard
-        if(Str::contains(request()->getHost(), 'staging') ) {
+        $requestDomain = request()->getHost();
+        $requestDomain = self::parseDomain($requestDomain);
+        $subdomain = Str::before($requestDomain, '.'); //wholesales-admin-staging.admin.dashboard
+        if(Str::contains($requestDomain, 'staging') ) {
             $subdomain.=".admin.";
-        } else if(Str::contains(request()->getHost(), 'test')) {
-            if(Str::contains(request()->getHost(), 'wholesales')) {
+        } else if(Str::contains($requestDomain, 'test')) {
+            if(Str::contains($requestDomain, 'wholesales')) {
                 $subdomain = "wholesales.admin.";
-            } else if(Str::contains(request()->getHost(), 'supermarket')) {
+            } else if(Str::contains($requestDomain, 'supermarket')) {
                 $subdomain = "supermarket.";
             } else {
                 $subdomain.=".";
@@ -121,5 +123,38 @@ class ApplicationEnvironment
     public static function isFrontEndEnvironment() : bool
     {
         return self::$type === "Frontend";
+    }
+
+
+
+    public static function parseDomain(string $requestDomain) : string
+    {
+        $domain = [
+            'wa.psgdc.store' => 'wa.psgdc.store',
+            'sa.psgdc.store' => 'sa.psgdc.store',
+            'wholesales.psgdc.store' => 'wholesales.psgdc.store',
+            'supermarket.psgdc.store' => 'supermarket.psgdc.store',
+            'psgdc.store' => 'psgdc.store',
+            'auth.psgdc.store' => 'auth.psgdc.store',
+            'admin.psgdc.store' => 'admin.psgdc.store',
+            'pa.psgdc.store' => 'pa.psgdc.store',
+            'rep.psgdc.store' => 'rep.psgdc.store',
+
+
+            'wa.generaldrugcentre.com' => 'wa.psgdc.store',
+            'sa.generaldrugcentre.com' => 'sa.psgdc.store',
+            'wholesales.generaldrugcentre.com' => 'wholesales.psgdc.store',
+            'supermarkets.generaldrugcentre.com' => 'supermarket.psgdc.store',
+            'supermarket.generaldrugcentre.com' => 'supermarket.psgdc.store',
+            'generaldrugcentre.com' => 'psgdc.store',
+            'www.generaldrugcentre.com' => 'psgdc.store',
+            'auth.generaldrugcentre.com' => 'auth.psgdc.store',
+            'admin.generaldrugcentre.com' => 'admin.psgdc.store',
+            'pa.generaldrugcentre.com' => 'pa.psgdc.store',
+            'rep.generaldrugcentre.com' => 'rep.psgdc.store',
+
+        ];
+
+        return $domain[$requestDomain] ?? $requestDomain;
     }
 }
