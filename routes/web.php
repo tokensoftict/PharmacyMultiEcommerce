02a081a4aa9b6d3794a09f952715ca9d6213675d
Yusuf Olatunji <yoyforever@gmail.com>
2026-07-02 21:24:50 +0100

    adding /app

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,28 @@ foreach (config('app.MAIN_DOMAIN', []) as $domain) {
         Route::view('/coupon-terms', 'coupon_terms')->name('coupon.terms');
         Volt::route('/', 'pages.frontend.customer.index')->name('customer.index');
         //Route::get('/', [\App\Http\Controllers\HomePageController::class, 'index'])->name('customer.index');
+        Route::get('/app', function () {
+            $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
+            if (strpos($ua, 'android') !== false) {
+                // Android → Play Store
+                header("Location: https://play.google.com/store/apps/details?id=com.tokensoftict.psgdc");
+                exit;
+
+            } elseif (
+                strpos($ua, 'iphone') !== false ||
+                strpos($ua, 'ipad') !== false ||
+                strpos($ua, 'ipod') !== false
+            ) {
+                // iOS → App Store
+                header("Location: https://apps.apple.com/us/app/ps-gdc/id6741708076");
+                exit;
+
+            } else {
+                // Fallback (optional)
+                header("Location: https://generaldrugcentre.com/");
+                exit;
+            }
+        })->name('customer.app_download');
         Route::get('file-manager', 'App\Http\Controllers\Utilities\FileManagerController@index')->name('file-manager.index');
         Route::prefix('sales-representative')->name('sales-representative.')->group(function () {
             Volt::route('{token}/accept-invitation', 'pages.salesrep.accept_invitation')->name('sales_rep.accept-invitation');
